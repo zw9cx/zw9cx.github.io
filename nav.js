dce8a946b4f8c8292b26bebad79119a1d9dc9d6d
yutauruma <diameter.sine57@gmail.com>
2026-08-27 18:11:21 +0900

    Initial commit

---
+++ nav.js
@@ -0,0 +1,10 @@
+const navHTML = `
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="math.html">Math</a></li>
+  <li><a href="diary.html">Diary</a></li>
+  <li><a href="links.html">Links</a></li>
+</ul>
+`;
+
+document.getElementById("nav").innerHTML = navHTML;
---
+++ texMacro.js
@@ -0,0 +1,18 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],
+
+    macros: {
+      R: "{\\mathbb{R}}",
+      C: "{\\mathbb{C}}",
+      N: "{\\mathbb{N}}",
+      Z: "{\\mathbb{Z}}",
+      Q: "{\\mathbb{Q}}",
+
+      supp: "{\\operatorname{supp}}"
+    }
+  }
+};
